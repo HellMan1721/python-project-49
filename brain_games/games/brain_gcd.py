b88e9cec7342266d63fa5d0738d32b71143e3109
Matvei Ivtushok <matveiivtushok@Matveis-MacBook-Air.local>
2025-01-09 08:24:13 +0300

    quick push

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -7,6 +7,6 @@ DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 def gen_gamedata():
     number_1 = random.randint(1, 100)
     number_2 = random.randint(1, 100)
-    random_numbers = f"{num_1} {num_2}"
-    gcd_answer = f"{math.gcd(num_1, num_2)}"
+    random_numbers = f"{number_1} {number_2}"
+    gcd_answer = f"{math.gcd(number_1, number_2)}"
     return random_numbers, gcd_answer
