f7616ebef78ca77056070713ec7675351fa5db70
Matvei Ivtushok <matveiivtushok@Matveis-MacBook-Air.local>
2025-01-11 00:37:10 +0300

    quick push

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,8 +6,8 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def isPrime(n):
     if n <= 1:
         return False
-    for i in range(2, int(n**0.5) + 1, 6):
-        if n % i == 0 or n % (i + 2) == 0:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
